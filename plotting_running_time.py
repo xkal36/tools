95584242bba5e0f68aaff1aacd3d5f1a3b108ec8
Andrew Winterbotham <awinterbotham02@gmail.com>
2016-03-03 15:19:24 +0000

    Update plotting_running_time.py

--- plotting_running_time.py
+++ plotting_running_time.py
@@ -1,5 +1,7 @@
 """
-Counting the number of iterations in various mystery functions
+Counting the number of iterations in functions,
+useful for testing and examining a function 
+to reason about its running time.
 """
 
 import simpleplot
@@ -12,8 +14,24 @@ LOGLOG = False
 # global counter that records the number of iterations for inner loops
 counter = 0
 
-###############################################
-# three mystery functions
+# Function that plots the number of iterations in a function
+def build_plot(plot_size, plot_function, plot_type=STANDARD):
+    """
+    Build plot of the number of increments in mystery function
+    """
+    global counter
+    plot = []
+    for input_val in range(2, plot_size):
+        counter = 0
+        plot_function(input_val)
+        if plot_type == STANDARD:
+            plot.append([input_val, counter])
+        else:
+            plot.append([math.log(input_val), math.log(counter)])
+    return plot
+
+# Example usage:
+
 def mystery1(input_val):
     """ 
     Function whose loops update global counter    
@@ -40,34 +58,15 @@ def mystery3(input_val):
     for index in range(input_val):
         for dummy_index in range(int(1.1 ** index)):
             counter += 1
-    
-def build_plot(plot_size, plot_function, plot_type = STANDARD):
-    """
-    Build plot of the number of increments in mystery function
-    """
-    global counter
-    plot = []
-    for input_val in range(2, plot_size):
-        counter = 0
-        plot_function(input_val)
-        if plot_type == STANDARD:
-            plot.append([input_val, counter])
-        else:
-            plot.append([math.log(input_val), math.log(counter)])
-    return plot
 
 
-
-###############################################
 # plottting code
 plot_type = STANDARD
 plot_size = 40
 
-# Pass name of mystery function in as a parameter
+# Pass name of function in as a parameter
 plot1 = build_plot(plot_size, mystery1, plot_type)
 plot2 = build_plot(plot_size, mystery2, plot_type)
 plot3 = build_plot(plot_size, mystery3, plot_type)
 simpleplot.plot_lines("Iteration counts", 600, 600, 
                       "input", "counter", [plot1, plot2, plot3])
-
-
